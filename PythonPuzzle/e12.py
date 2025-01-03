d870e05a974156c9fa1a2dcc9653072e8ecaa9e7
Weaver <cordies1106@gmail.com>
2025-01-03 19:58:06 +0200

    exercises

---
+++ PythonPuzzle/e12.py
@@ -0,0 +1,13 @@
+# Test whether the given strings are palindromes
+
+def test(strs):
+    return [s == s[::-1] for s in strs]
+
+
+strs = ['palindrome', 'madamimadam', '', 'foo', 'eyes']
+
+print("Original strings:")
+print(strs)
+
+print("\nTest whether the given strings are palindromes or not:")
+print(test(strs))
