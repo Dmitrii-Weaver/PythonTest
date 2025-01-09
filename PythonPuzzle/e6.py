d26b86fd5a8c4120ae7e47abbf324cdfc838d940
Weaver <cordies1106@gmail.com>
2025-01-09 19:50:13 +0200

    exercises

---
+++ PythonPuzzle/e6.py
@@ -0,0 +1,21 @@
+# Find a list of one hundred integers between 0 and 999 which all differ by ten from one another
+
+def test(li):
+    return all(i in range(1000) and abs(i - j) >= 10 for i in li for j in li if i != j) and len(set(li)) == 100
+
+
+nums = list(range(0, 1000, 10))
+
+print("Original list:")
+print(nums)
+
+print("Check whether the said list contains one hundred integers between 0 and 999 which all differ by ten from one another:")
+print(test(nums))
+
+nums = list(range(0, 1000, 20))
+
+print("\nOriginal list:")
+print(nums)
+
+print("Check whether the said list contains one hundred integers between 0 and 999 which all differ by ten from one another:")
+print(test(nums))
