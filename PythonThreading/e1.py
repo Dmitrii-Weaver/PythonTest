2ae7bdc0aa1f365078b042b5957967a9ae382c7d
Weaver <cordies1106@gmail.com>
2025-03-05 18:35:55 +0200

    Threading exercises

---
+++ PythonThreading/e1.py
@@ -0,0 +1,17 @@
+# Write a Python program to create multiple threads and print their names.
+
+import threading
+
+
+def print_thread_names():
+    print("Current thread name:", threading.current_thread().name)
+
+
+threads = []
+for i in range(7):
+    thread = threading.Thread(target=print_thread_names)
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
